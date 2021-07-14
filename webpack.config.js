084c348971e3dac20fe3e16ff5d3775965458296
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-07-14 14:36:45 -0400

    Setup local dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
